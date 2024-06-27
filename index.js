806e2ea1edf427131371975ab2f3c2d0d7bd1220
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-06-27 04:23:04 +0300

    introduction to function

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Code your solution in this file!
+let hqStreet=42;
+let distanceInBlocks=0;
+let block=264;
+let distInFeet=0;
+let dist=0;
+let blocksTravelled=0;
+let fare=0;
+
+function distanceFromHqInBlocks(pickUpStreet){
+    if(hqStreet>pickUpStreet){
+        distanceInBlocks=hqStreet-pickUpStreet;
+        return distanceInBlocks;
+    } else if(pickUpStreet>hqStreet){
+        distanceInBlocks=pickUpStreet-hqStreet;
+        return distanceInBlocks;
+    } 
+}
+
+function distanceFromHqInFeet(pickUpStreet){
+
+    return distanceFromHqInBlocks(pickUpStreet) *block;
+    
+    }
+
+console.log(distanceFromHqInFeet(9));
+
+
+
+
+function distanceTravelledInFeet(start, destination){
+    if (destination > start){
+        blocksTravelled=(destination-start)*264
+        return blocksTravelled;
+    } else if (destination < start)
+        blocksTravelled=(start-destination)*264;
+        return blocksTravelled;
+    }
+    
+
+
+function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    let distance=distanceTravelledInFeet(start,destination);
+    if (distance<=400){
+        return 0;
+
+    } else if (distance>400 && distance<2000){
+        fare =(distance-400)*0.02
+        return fare;
+    }else if (distance>2000 && distance<2500){
+        fare=25;
+        return fare;
+    }else if (distance>2500){
+        return 'cannot travel that far'
+    }
+}
